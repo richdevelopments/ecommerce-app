e4a913eff930c78118b7527238397e64fc98a8f9
Ubuntu <ubuntu@ip-172-31-37-44.ec2.internal>
2019-07-31 09:10:42 +0000

    update settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-# import env
+import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
-    'storages',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -91,6 +91,9 @@ else:
         }
     }
 
+
+# Password validation
+# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
